8486a65d7be2eee692374e0c6fc8f33df426a0b4
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-07-02 20:51:30 -0700

    refactored to add classes and debugged

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import { Triangle } from './triangle.js';
 
 $(document).ready(function() {
   $("form#triangle").submit(function(event) {
-    var side1 = parseInt($("input#side1").val());
-    var side2 = parseInt($("input#side2").val());
-    var side3 = parseInt($("input#side3").val());
-    var triangle = new Triangle(side1, side2, side3);
-    var result = triangle.checkType();
+    let side1 = parseInt($("input#side1").val());
+    let side2 = parseInt($("input#side2").val());
+    let side3 = parseInt($("input#side3").val());
+    let triangle = new Triangle(side1, side2, side3);
+    let result = triangle.checkType();
 
-    $(".triangle_type").text(result);
+    $(".triangle_type").text(`This is ${result}`);
 
     $("#result").show();
 
